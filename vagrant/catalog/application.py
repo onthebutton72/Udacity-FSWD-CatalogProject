a85677dc704ba519c2c97e74c2597967f64dfce8
Jamie Martinez <martinejamie@hotmail.com>
2019-04-12 17:29:48 -0700

    Update to create database, connect to database and output movie to web interface.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,11 +1,33 @@
-# Import Flask
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Table
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql:///catalog')
+metadata = MetaData()
+
+Genres = Table('genres', metadata, autoload=True, autoload_with=engine)
+Movies = Table('movies', metadata, autoload=True, autoload_with=engine)
+
+DBSession = sessionmaker(bind=engine)
+Base = declarative_base()
+Session = sessionmaker(engine)
+session = Session()
+
 app = Flask(__name__)
 
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/movies')
+def getData():
+	genres = session.query(Genres)[1]
+	movies = session.query(Movies).filter_by(genre_id = genres.id)
+	output = ''
+	for i in movies:
+		output += i.title
+		output += '</br>'
+	return output
 
 if __name__ == '__main__':
 	app.debug = True
