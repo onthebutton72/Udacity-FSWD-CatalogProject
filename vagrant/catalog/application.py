7d1cf10830677063d2375da00bc9d57ced07b98a
Jamie Martinez <martinejamie@hotmail.com>
2019-04-15 09:12:00 -0700

    change getGenres function name to genresMenu

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/genres/<int:genre_id>/')
-def getGenres(genre_id):
+def genreMenu(genre_id):
 	genres = session.query(Genres).filter_by(id=genre_id).one()
 	movies = session.query(Movies).filter_by(genre_id = genre_id)
 	output = ''
