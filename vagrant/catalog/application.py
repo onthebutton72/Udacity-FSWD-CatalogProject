f377c874f0f33a68a48aaebd7ea34b4ec51c021a
Jamie Martinez <martinejamie@hotmail.com>
2019-04-15 16:10:35 -0700

    Add Delete Menu Item.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -48,9 +48,15 @@ def editMovieItem(genre_id, movie_id):
 
 
 #Create a route for deleteMovieItem function
-@app.route('/genres/<int:genre_id>/<int:movie_id>/delete/')
+@app.route('/genres/<int:genre_id>/<int:movie_id>/delete/', methods=['GET', 'POST'])
 def deleteMovieItem(genre_id, movie_id):
-	return "page to delete a new movie item.  Task 3 complete!"
+	itemToDelete = session.query(Movies).filter_by(id = movie_id).one()
+	if request.method == 'POST':
+		session.delete(itemToDelete)
+		session.commit()
+		return redirect(url_for('genreMenu', genre_id = genre_id))
+	else:
+		return render_template('deletemovieitem.html', i = itemToDelete)
 
 
 if __name__ == '__main__':
