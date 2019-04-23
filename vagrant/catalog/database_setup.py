13a713364360e6a5f43edb3656bbf8e4ec61458b
Jamie Martinez <martinejamie@hotmail.com>
2019-04-23 10:37:35 -0700

    Updated double quotations with single

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -41,11 +41,11 @@ session = Session()
 Base.metadata.create_all(db)
 
 # Create Genres Table
-comedy = Genres(name="Comedy")
-horror = Genres(name="Horror")
-action = Genres(name="Action")
-drama = Genres(name="Drama")
-romance = Genres(name="Romance")
+comedy = Genres(name='Comedy')
+horror = Genres(name='Horror')
+action = Genres(name='Action')
+drama = Genres(name='Drama')
+romance = Genres(name='Romance')
 
 session.add(comedy)
 session.add(horror)
@@ -57,9 +57,9 @@ session.commit()
 # Create Movies Table
 
 shining = (
-    "A family heads to an isolated hotel for the winter where a "
-    "sinister presence influences the father into violence, while his pyschic"
-    "son sees horrific forebodings from both past and future."
+    'A family heads to an isolated hotel for the winter where a '
+    'sinister presence influences the father into violence, while his pyschic'
+    'son sees horrific forebodings from both past and future.'
     )
 
 the_shining = Movies(
@@ -69,9 +69,9 @@ the_shining = Movies(
     )
 
 red_october = (
-    "In November 1984 the Soviet Unions best submarine captain "
-    "violates orders and heads for the US either trying to defect or start a "
-    "war."
+    'In November 1984 the Soviet Unions best submarine captain '
+    'violates orders and heads for the US either trying to defect or start a '
+    'war.'
     )
 
 the_hunt_for_red_october = Movies(
@@ -81,9 +81,9 @@ the_hunt_for_red_october = Movies(
     )
 
 notebook = (
-    "A poor yet passionate young man falls in love with a rich young "
-    " woman, giving her a sense of freedom, but they are soon separated "
-    "because of their social differences."
+    'A poor yet passionate young man falls in love with a rich young '
+    ' woman, giving her a sense of freedom, but they are soon separated '
+    'because of their social differences.'
     )
 
 the_notebook = Movies(
@@ -93,9 +93,9 @@ the_notebook = Movies(
     )
 
 other_guys = (
-    "Two mismatched New York City detectives seize an opportunity "
-    "to step up like the top cops, whom they idolize, only things do not "
-    "quite go as planned."
+    'Two mismatched New York City detectives seize an opportunity '
+    'to step up like the top cops, whom they idolize, only things do not '
+    'quite go as planned.'
     )
 
 the_other_guys = Movies(
@@ -105,9 +105,9 @@ the_other_guys = Movies(
     )
 
 man = (
-    "A look at the life of the astronaut, Neil Armstrong, and the "
-    "legendary space mission that led him to become the first man to walk on "
-    "the Moon on July 20, 1969."
+    'A look at the life of the astronaut, Neil Armstrong, and the '
+    'legendary space mission that led him to become the first man to walk on '
+    'the Moon on July 20, 1969.'
     )
 
 first_man = Movies(
